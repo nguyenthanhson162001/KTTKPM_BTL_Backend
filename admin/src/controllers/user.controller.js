5ee00372099e76a374e52eb041f153b910a765bb
cong85010 <cong85010@gmail.com>
2022-11-17 15:41:29 +0700

    update authencation

--- admin/src/controllers/user.controller.js
+++ admin/src/controllers/user.controller.js
@@ -1,6 +1,28 @@
 const User = require("../models/user.model");
 
 module.exports = {
+ 
+  async disableAdmin(req, res, next) {
+    const email = req.params.email;
+    let user = await User.findOne({ email: email });
+    if (!user) {
+      return res.json({
+        sucsess: false,
+        message: "email not found",
+      });
+    }
+    if (!user.auth.isAdmin) {
+      return res.json({
+        sucsess: false,
+        message: "the user not is an administrator",
+      });
+    }
+    await User.update({ email: email }, { "auth.isAdmin": false });
+    return res.json({
+      sucsess: true,
+      message: `disable admin for user ${email} success`,
+    });
+  },
   async getStatisticalUser(req, res, next) {
     const sum = await User.count({});
     res.json({ sum });
@@ -24,27 +46,6 @@ module.exports = {
     return res.json({
       sucsess: true,
       message: `set admin for user ${email} success`,
-    });
-  },
-  async disableAdmin(req, res, next) {
-    const email = req.params.email;
-    let user = await User.findOne({ email: email });
-    if (!user) {
-      return res.json({
-        sucsess: false,
-        message: "email not found",
-      });
-    }
-    if (!user.auth.isAdmin) {
-      return res.json({
-        sucsess: false,
-        message: "the user not is an administrator",
-      });
-    }
-    await User.update({ email: email }, { "auth.isAdmin": false });
-    return res.json({
-      sucsess: true,
-      message: `disable admin for user ${email} success`,
     });
   },
 };
