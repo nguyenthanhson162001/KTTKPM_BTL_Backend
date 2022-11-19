e661e98611eb8e2f8a3a84c93e8f7c5bb95286cd
cong85010 <cong85010@gmail.com>
2022-11-19 15:43:26 +0700

    update redis

--- source/src/configs/redis.js
+++ source/src/configs/redis.js
@@ -2,11 +2,7 @@ const util = require('util');
 const redis = require('redis');
 const dotenv = require('dotenv').config();
 
-// 6379
-// const client = redis.createClient({
-//     host: process.env.REDIS_HOST,
-//     port: process.env.REDIS_PORT,
-// });
+
 const client = redis.createClient({
     port: 6379,
     host: "127.0.0.1",
@@ -22,6 +18,12 @@ client.on('error', function (error) {
     console.error('Redis Error: ', error);
 });
 
+// 6379
+// const client = redis.createClient({
+//     host: process.env.REDIS_HOST,
+//     port: process.env.REDIS_PORT,
+// });
+
 const setClient = util.promisify(client.set).bind(client);
 const getClient = util.promisify(client.get).bind(client);
 const existsClient = util.promisify(client.exists).bind(client);
@@ -30,16 +32,16 @@ const set = async (key, value) => {
     await setClient(key, JSON.stringify(value));
 };
 
-const get = async (key) => {
-    const data = await getClient(key);
-
-    return JSON.parse(data);
-};
-
 const exists = async (key) => {
     const isExists = await existsClient(key);
 
     return isExists === 1;
+};
+
+const get = async (key) => {
+    const data = await getClient(key);
+
+    return JSON.parse(data);
 };
 
 module.exports = {
