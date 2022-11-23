86af142c05217c74fa0cce024db657f64bc4a670
NGUYỄN THANH SƠN <nguyenthanhson162001@gmail.com>
2022-11-23 22:27:20 +0700

    config proxy socket

--- proxy/index.js
+++ proxy/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+require("express-async-errors");
 const port = process.env.PORT | 5001;
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const helmet = require("helmet");
@@ -7,33 +8,53 @@ var morgan = require("morgan");
 
 // proxy middleware options
 /** @type {import('http-proxy-middleware/dist/types').Options} */
-const url = process.env.TARGET | "http://localhost:5000";
+const url = process.env.TARGET;
 const serverAPI = process.env.URL_API;
 const serverAdmin = process.env.URL_ADMIN;
+// const wsProxy = createProxyMiddleware({
+//   target: "http://ws.ifelse.io",
+//   changeOrigin: true, // for vhosted sites, changes host header to match to target's host
+//   ws: true, // enable websocket proxy
+//   logger: console,
+//   router: {
+//     "/api": serverAPI,
+//   },
+// });
 const options = {
   target: url, // target host
   changeOrigin: true, // needed for virtual hosted sites
-  ws: true, // proxy websockets
+  ws: true,
+  logger: console,
   pathRewrite: {
     "^/admin": "/", // rewrite path
+    "^/": "/", // rewrite path
   },
   router: {
-    "/api": serverAPI,
+    "/": serverAPI,
     "/admin": serverAdmin,
+  },
+  onProxyReqWs: (proxyReq, req, socket) => {
+    socket.on("error", function (error) {
+      console.warn("Websockets error.", error);
+    });
   },
 };
 // create the proxy (without context)
-const exampleProxy = createProxyMiddleware(options);
+const proxy = createProxyMiddleware(options);
 
 // mount `exampleProxy` in web server
 const app = express();
 app.use(morgan("tiny"));
 
 app.use(helmet());
-app.use("/", exampleProxy);
+app.use(proxy);
+// app.use(wsProxy);
 
 app.use("*", (req, res, next) => {
   res.send("url not found: this is server proxy");
 });
 
-app.listen(port, () => console.log(`proxy start on port ${port}   `));
+const server = app.listen(port, () =>
+  console.log(`proxy start on port ${port}   `)
+);
+server.on("upgrade", proxy.upgrade); // optional: upgrade externally
